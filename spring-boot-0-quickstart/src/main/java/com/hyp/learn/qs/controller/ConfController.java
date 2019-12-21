5e58b7ec16a2c5bc950b28f9051e249baaba3ebe
hanyunpeng <m13839441583@163.com>
2019-12-21 17:30:30 +0800

    first commit

---
+++ spring-boot-0-quickstart/src/main/java/com/hyp/learn/qs/controller/ConfController.java
@@ -0,0 +1,39 @@
+package com.hyp.learn.qs.controller;
+
+
+import com.hyp.learn.qs.conf.DefinitionConfig;
+import com.hyp.learn.qs.conf.DubboConfig;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class ConfController {
+	
+	@Resource
+	DefinitionConfig conf;
+	
+	@Resource
+	DubboConfig dubbo;
+	
+	/**
+	 * 测试自定义配置属性加载
+	 * @return
+	 */
+	@RequestMapping("/conf")
+	public String getConfig() {
+		System.out.println(conf.toString());
+		return conf.toString();
+	}
+	
+	/**
+	 * 测试自定义的额外文件的配置信息
+	 * @return
+	 */
+	@RequestMapping("/dubbo")
+	public String dubboConfig() {
+		System.out.println(dubbo.toString());
+		return dubbo.toString();
+	}
+}
---
+++ spring-boot-0-quickstart/src/main/java/com/hyp/learn/qs/controller/HelloWorldController.java
@@ -0,0 +1,31 @@
+package com.hyp.learn.qs.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hyp
+ * Project name is spring-boot-learn
+ * Include in com.hyp.learn.qs.controller
+ * hyp create at 19-12-21
+ **/
+@RestController
+public class HelloWorldController {
+    /**
+     * The class is flagged as a @RestController,meaning it’s ready for use by Spring MVC to handle web requests.
+     *
+     * @RequestMapping maps / to the index() method. When invoked(调用) from a browser or using curl on the command line,
+     * the method returns pure(单纯的) text.
+     * <p>
+     * That’s because @RestController combines(结合) @Controller and @ResponseBody,
+     * two annotations that results in web requests returning data rather than a view.
+     * <p>
+     * 那是因为@RestController联合了@Controller and @ResponseBody两个注解导致返回的是数据而不是视图
+     */
+
+    @RequestMapping("/hello")
+    public String index() {
+        System.out.println("Hello Spring-Boot");
+        return "Hello World";
+    }
+}
