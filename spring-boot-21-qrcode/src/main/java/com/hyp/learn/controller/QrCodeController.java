2e410cb714de013928f13015d149b8bd42116500
hanyunpeng <m13839441583@163.com>
2020-03-25 16:08:32 +0800

    增加二维码、websocket模块

---
+++ spring-boot-21-qrcode/src/main/java/com/hyp/learn/controller/QrCodeController.java
@@ -0,0 +1,60 @@
+package com.hyp.learn.controller;
+
+import com.hyp.learn.utils.QRCodeUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+
+/**
+ * @author hyp
+ * Project name is spring-boot-learn
+ * Include in com.hyp.learn.controller
+ * hyp create at 20-3-9
+ **/
+@RestController
+public class QrCodeController {
+    /**
+     * 根据 url 生成 普通二维码
+     */
+    @RequestMapping(value = "/createCommonQRCode")
+    public void createCommonQRCode(HttpServletResponse response, String url) throws Exception {
+        ServletOutputStream stream = null;
+        try {
+            stream = response.getOutputStream();
+            //使用工具类生成二维码
+            QRCodeUtil.encode(url, stream);
+        } catch (Exception e) {
+            e.getStackTrace();
+        } finally {
+            if (stream != null) {
+                stream.flush();
+                stream.close();
+            }
+        }
+    }
+
+    /**
+     * 根据 url 生成 带有logo二维码
+     */
+    @RequestMapping(value = "/createLogoQRCode")
+    public void createLogoQRCode(HttpServletResponse response, String url) throws Exception {
+        ServletOutputStream stream = null;
+        try {
+            stream = response.getOutputStream();
+            String logoPath = Thread.currentThread().getContextClassLoader().getResource("").getPath()
+                    + "templates" + File.separator + "logo.jpg";
+            //使用工具类生成二维码
+            QRCodeUtil.encode(url, logoPath, stream, true);
+        } catch (Exception e) {
+            e.getStackTrace();
+        } finally {
+            if (stream != null) {
+                stream.flush();
+                stream.close();
+            }
+        }
+    }
+}
---
+++ spring-boot-22-websocket/src/main/java/com/hyp/learn/websocket/conf/WebSocketConfig.java
@@ -0,0 +1,52 @@
+package com.hyp.learn.websocket.conf;
+
+
+import com.hyp.learn.websocket.common.PrincipalHandshakeHandler;
+import com.hyp.learn.websocket.common.WebSocketDecoratorFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+
+/**
+ * WebSocketConfig配置
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Autowired
+    private WebSocketDecoratorFactory webSocketDecoratorFactory;
+    @Autowired
+    private PrincipalHandshakeHandler principalHandshakeHandler;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        /**
+         * myUrl表示 你前端到时要对应url映射
+         */
+        registry.addEndpoint("/myUrl")
+                .setAllowedOrigins("*")
+                .setHandshakeHandler(principalHandshakeHandler)
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        /**
+         * queue 点对点
+         * topic 广播
+         * user 点对点前缀
+         */
+        registry.enableSimpleBroker("/queue", "/topic");
+        registry.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.addDecoratorFactory(webSocketDecoratorFactory);
+    }
+}
